368dfc085be28e0b6712a7d10ea3863005cee232
vlasashk <79715336+vlasashk@users.noreply.github.com>
2022-12-28 02:02:08 +0300

    Delete .ccls-cache directory

--- .ccls-cache/@home@runner@StringPlus/s21_sprintf.c
+++
@@ -1,7 +0,0 @@
-#include "s21_string.h"
-
-int s21_sprintf(char *str, const char *format, ...) {
-  (void)str;
-  (void)format;
-  return 0;
-}
--- .ccls-cache/@home@runner@StringPlus/test.c
+++
@@ -1,8 +0,0 @@
-#include "s21_string.h"
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-  printf("%s\n", strerror(-4));
-  return 0;
-}
